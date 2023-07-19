d1c6b5dd23530cf875ca82bdd406510dc9221ce2
marartenian <artenianmar@gmail.com>
2023-07-19 16:09:51 +0300

    edited index.html and deleted menu cards from static html, and then added cards from JavaScript ES6 Classes

--- js/index.js
+++ js/index.js
@@ -121,7 +121,7 @@ window.addEventListener("DOMContentLoaded", function (){
                 }
             });
 
-            const modalTimerId = setTimeout(openModal,600000);
+            // const modalTimerId = setTimeout(openModal,600000);
 
             function showModalByScroll(){
                     if(window.scrollY >= 2000){
@@ -136,7 +136,7 @@ window.addEventListener("DOMContentLoaded", function (){
             modal.classList.add("show");
             modal.classList.remove("hide");
             document.body.style.overflow = "hidden";
-            clearTimeout(modalTimerId);
+            // clearTimeout(modalTimerId);
         }
         function closeModal(){
             modal.classList.remove("show");
@@ -145,5 +145,72 @@ window.addEventListener("DOMContentLoaded", function (){
         }
 
     //modal logic end
+    
+    // used  Class for menu cards => start
+
+    class MenuCard{
+        constructor(img, alt, title, descr, price, parentSelector){
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH (){
+            this.price = this.price * this.transfer;
+        }
+
+        render(){
+            const {img, alt, title, descr, price, parent}= this
+            const element = document.createElement("div");
+            element.classList.add("menu__item");
+            element.innerHTML = `
+            <img src=${img}  alt=${alt}>
+            <h3 class="menu__item-subtitle">${title}</h3>
+            <div class="menu__item-descr">${descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${price}</span> грн/день</div>
+            </div>
+            `;
+
+            parent.append(element);
+        }
+    }
+
+     new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+       " Меню \"Фитнес\"",
+       "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+       8.5,
+       ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню \“Премиум\”",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        20.4,
+       ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        "Меню \"Постное\"",
+        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        16,
+       ".menu .container"
+    ).render();
+
+
+    // used  Class for menu cards => end
     
 });
